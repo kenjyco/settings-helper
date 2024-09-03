92293f6c56a8d4a321d6715117eb040c39cf904f
Ken <kenjyco@gmail.com>
2024-09-03 15:18:30 -0500

    Bump to v0.0.13

--- setup.py
+++ setup.py
@@ -9,14 +9,14 @@ with open('requirements.txt', 'r') as fp:
 
 setup(
     name='settings-helper',
-    version='0.0.12',
+    version='0.0.13',
     description='Helpers to get specific settings from a particular section of a settings.ini file',
     long_description=long_description,
     author='Ken',
     author_email='kenjyco@gmail.com',
     license='MIT',
     url='https://github.com/kenjyco/settings-helper',
-    download_url='https://github.com/kenjyco/settings-helper/tarball/v0.0.12',
+    download_url='https://github.com/kenjyco/settings-helper/tarball/v0.0.13',
     packages=find_packages(),
     setup_requires=['pytest-runner'],
     tests_require=['pytest'],
